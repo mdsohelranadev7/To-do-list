22aae3e6e21211ca92a46a2199c8db813621e134
Sohel Rana <mdsohelrana191807@gmail.com>
2026-09-17 20:35:43 +0600

    sfddsadfsafd

--- js/script.js
+++ js/script.js
@@ -1,45 +1,90 @@
-let input = document.getElementById('input')
-let addButton = document.getElementById('addButton')
 
+let inp = document.getElementById("inp")
+let addbtn = document.getElementById("addbtn")
+let list = document.getElementById("list")
 
-addButton.addEventListener('click', () => {
-    let inputvalue = input.value.trim()
+addbtn.addEventListener('click', () => {
+    let htel = inp.value
 
-    if (inputvalue == "") {
-
+    if (htel == "")
         alert('Enter your task')
 
-    }
     else {
+        let hssd = document.createElement('li')
+        hssd.innerHTML = htel + ' <button onclick="ddd(this)">Delete</button>'
 
-        let ullist = document.getElementById('ullist')
-        let li = document.createElement('li')
+        list.appendChild(hssd)
 
-        li.innerHTML = '<input type="checkbox">  ' + inputvalue + '<button class="ds" onclick="skk(this)">Delete</button>'
-
-        ullist.appendChild(li)
-
-        input.value = ""
-
+        inp.value = ""
     }
 
 })
 
-// let dkfds = document.getElementById('dkfds')
-
-
-// dkfds.addEventListener('click', () => {
-
-//   ullist.innerHTML = ""
-// })
-
-
-
-let skk = (bts) => {
-
-
-    let lis = bts.parentNode
-    lis.parentNode.removeChild(lis)
-
+function ddd(ssd) {
+    ssd.parentElement.remove()
 
 }
+
+
+
+
+let start = document.getElementById("start")
+let stop = document.getElementById("stop")
+let timer = document.getElementById("timer")
+let inputss = document.getElementById("inputss")
+
+let dsds =0
+let usertime = 0
+
+
+let delinee;
+
+let startfun = () => {
+    if (dsds <= 0) {
+        dsds = usertime
+    }
+
+
+    clearInterval(delinee)
+
+    delinee = setInterval(() => {
+
+        let minit = parseInt(dsds / 60, 10)
+        let secound = parseInt(dsds % 60, 10)
+
+
+        timer.textContent = minit + ":" + secound
+
+        if (dsds-- <= 0) {
+            clearInterval(delinee)
+        }
+
+
+
+    }, 1000);
+
+}
+
+
+
+start.addEventListener('click', () => {
+    let inptins = parseInt(inputss.value, 10)
+    if (!isNaN(inptins) && inptins > 0) {
+        usertime = inptins * 60
+        dsds = usertime
+        inputss.value = ""
+
+    }
+
+
+    else{
+        alert('enter your minit')
+    }
+
+    startfun()
+})
+
+
+stop.addEventListener('click', () => {
+    clearInterval(delinee)
+})
